445d17ce3ce113a7e9fb14f0dda3d0bb3788d65c
gilles Bertrand <gilles@pop-os.localdomain>
2020-04-09 16:25:27 +0200

    First Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a base project for node and typescript development
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+interface Msg {
+  name: string;
+  surName: string;
+}
+const msg: Msg = {
+  name: 'gillsssses',
+  surName: 'testee',
+};
+console(msg);
